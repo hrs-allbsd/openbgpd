0c55e484461fe070f9d2d2978964c911f68dcd4c
Hiroki Sato <hrs@allbsd.org>
2019-03-03 16:11:02 +0900

    Add definition of timespecsub macro.

--- openbsd-compat/openbsd-compat.h
+++ openbsd-compat/openbsd-compat.h
@@ -89,6 +89,13 @@ struct if_status_description {
 	{ 0, 0, NULL }							\
 }
 
-#include <sys/timespec.h>
-void timespecsub(struct timespec *, struct timespec *, struct timespec *);
+#define timespecsub(tsp, usp, vsp)                                      \
+        do {                                                            \
+                (vsp)->tv_sec = (tsp)->tv_sec - (usp)->tv_sec;          \
+                (vsp)->tv_nsec = (tsp)->tv_nsec - (usp)->tv_nsec;       \
+                if ((vsp)->tv_nsec < 0) {                               \
+                        (vsp)->tv_sec--;                                \
+                        (vsp)->tv_nsec += 1000000000L;                  \
+                }                                                       \
+        } while (0)
 #endif /* _OPENBSD_COMPAT_H */
